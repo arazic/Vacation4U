94fa39b8bf8d5b3787e386f8858743665f95353f
arazi_chen <arazic@post.bgu.ac.il>
2018-12-10 20:14:39 +0200

    universal

--- src/main/java/View/System.java
+++ src/main/java/View/System.java
@@ -19,7 +19,7 @@ import java.time.LocalDate;
 import java.util.*;
 
 public class System {
-
+//..
     @FXML
     public javafx.scene.control.Button btn_create;
     public javafx.scene.control.TextField txtfld_userName;
